b6575fcd1114fa88babff48e0aca89f1b7c7cb18
yara-elmohtady <128075776+yara-elmohtady@users.noreply.github.com>
2023-06-05 11:47:40 -1000

    file

--- software/Drivers/Test_PWM/Test_PWM.ino
+++ software/Drivers/Test_PWM/Test_PWM.ino
@@ -2,21 +2,21 @@
 
 
 void setup() {
-pinMode(16,OUTPUT) ; // Motor in1
-pinMode(5,OUTPUT) ; //  Motor in2
-Serian.begin(9600);
+pinMode(D0,OUTPUT) ; // Motor in1
+pinMode(D1,OUTPUT) ; //  Motor in2
+Serial.begin(9600);
 }
 
 void loop() {
 
   Serial.println("Motor Move Forward") ;
-  digitalWrite(16,HIGH) ; // DIR Motor 1
-  digitalWrite(5,HIGH) ; // DIR Motor 2
+  digitalWrite(D0,HIGH) ; // DIR Motor 1
+  digitalWrite(D1,LOW) ; // DIR Motor 2
   
   delay(1000);
   Serial.println("Motor Move Backword") ;
-  digitalWrite(16,LOW) ; // DIR Motor 1
-  digitalWrite(5,LOW) ; // DIR Motor 2
+  digitalWrite(D0,HIGH) ; // DIR Motor 1
+  digitalWrite(D1,LOW) ; // DIR Motor 2
   delay(1000);
 
 }
