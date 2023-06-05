57980e90810490f945945d14ae435acd8ca6e101
mahmamdouh <mahmoud.elmohtady@gmail.com>
2023-06-06 00:12:51 +0200

    driver test

--- software/Drivers/Test_PWM/Test_PWM.ino
+++ software/Drivers/Test_PWM/Test_PWM.ino
@@ -1,32 +1,39 @@
 
 
 
+// in1,4 forward 
+// in2,3 backword
+
 void setup() {
 pinMode(D3,OUTPUT) ; // Motor in1
 pinMode(D4,OUTPUT) ; //  Motor in2
-Serian.begin(9600);
+pinMode(D5,OUTPUT) ; // Motor in3
+pinMode(D6,OUTPUT) ; //  Motor in4
+Serial.begin(9600);
 
 }
 
 void loop() {
 
 
-  Serial.println("Option1") ;
-  digitalWrite(D3,HIGH) ; // DIR Motor 1
-  digitalWrite(D4,HIGH) ; // DIR Motor 2
+  Serial.println("Motor stop") ;
+  digitalWrite(D3,HIGH) ; // DIR Motor 1 motor stop
+  digitalWrite(D4,HIGH) ; // DIR Motor 2 motor stop
+  digitalWrite(D5,HIGH) ; // DIR Motor 1 motor stop
+  digitalWrite(D6,HIGH) ; // DIR Motor 2 motor stop
   
   delay(10000);
-  Serial.println("Option 2 ") ;
-  digitalWrite(D3,LOW) ; // DIR Motor 1
-  digitalWrite(D4,LOW) ; // DIR Motor 2
+  Serial.println("Forward") ;
+  digitalWrite(D3,HIGH) ; // DIR Motor 1 motor stop
+  digitalWrite(D4,LOW) ; // DIR Motor 2 motor stop
+  digitalWrite(D5,LOW) ; // DIR Motor 1 motor stop
+  digitalWrite(D6,HIGH) ; // DIR Motor 2 motor stop
   delay(10000);
-  Serial.println("Option 3") ;
-  digitalWrite(D3,HIGH) ; // DIR Motor 1
-  digitalWrite(D4,LOW) ; // DIR Motor 2
+  digitalWrite(D3,LOW) ; // DIR Motor 1 motor stop
+  digitalWrite(D4,HIGH) ; // DIR Motor 2 motor stop
+  digitalWrite(D5,HIGH) ; // DIR Motor 1 motor stop
+  digitalWrite(D6,LOW) ; // DIR Motor 2 motor stop
   delay(10000);
-  Serial.println("Option 4") ;
-  digitalWrite(D3,LOW) ; // DIR Motor 1
-  digitalWrite(D4,HIGH) ; // DIR Motor 2
-  delay(10000);
+
 
 }
