64d6f199a40d05bdd44694affc0681cc7167e6fb
yara-elmohtady <128075776+yara-elmohtady@users.noreply.github.com>
2023-06-09 14:11:17 -1000

    volage

--- software/Drivers/Ultrasonic_Driver/ultrasonic_test/Pinout.h
+++ software/Drivers/Ultrasonic_Driver/ultrasonic_test/Pinout.h
@@ -1,26 +1,7 @@
-// Pinout defenitions
-
-#define Motor_Driver_Right_Front_Dir 4
-#define Motor_Driver_Right_Front_PWM 5
-#define Motor_Driver_Right_Back_Dir 2
-#define Motor_Driver_Right_Back_PWM 0
-#define Motor_Driver_Left_front_Dir 12
-#define Motor_Driver_Left_front_PWM 14
-#define Motor_Driver_Left_Back_Dir 15
-#define Motor_Driver_Left_Back_PWM 13
 
 //Unltrasonic
-#define ULTRASONIC_F1_ECHO D0
-#define ULTRASONIC_F1_TRIGGER D1
-
-#define ULTRASONIC_F2_ECHO D2
-#define ULTRASONIC_F2_TRIGGER D3
-
-#define ULTRASONIC_F3_ECHO D4
-#define ULTRASONIC_F3_TRIGGER D5
+#define ULTRASONIC_F1_ECHO 15
+#define ULTRASONIC_F1_TRIGGER 13
 
 #define ULTRASONIC_B1_ECHO 12
 #define ULTRASONIC_B1_TRIGGER 14
-
-#define ULTRASONIC_B2_ECHO 15
-#define ULTRASONIC_B2_TRIGGER 13
--- software/Drivers/Ultrasonic_Driver/ultrasonic_test/ultrasonic_test.ino
+++ software/Drivers/Ultrasonic_Driver/ultrasonic_test/ultrasonic_test.ino
@@ -1,24 +1,59 @@
-#include "ultrasonic.h"
-
-
+#include "Pinout.h"
+#define SOUND_SPEED 0.034
+extern void Ultrasonic_init();
+float get_ultrasonic_reading(int Trigger ,int Echo);
 
 void setup() {
   // put your setup code here, to run once:
   Ultrasonic_init();
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  struct Ultrasonic_Distance Ultrasonic_distance ;
+  float Distnce ;
 
-  Ultrasonic_distance = Ultrasonic_Scan_Back() ;
+  Distnce = get_ultrasonic_reading(ULTRASONIC_F1_TRIGGER ,ULTRASONIC_F1_ECHO) ;
   Serial.println("Back Sensor 1:");
-  Serial.println(Ultrasonic_distance.Back_distance1);
+  Serial.println(Distnce);
 
+  Distnce = get_ultrasonic_reading(ULTRASONIC_B1_TRIGGER ,ULTRASONIC_B1_ECHO) ;
   Serial.println("Back Sensor 2:");
-  Serial.println(Ultrasonic_distance.Back_distance2);
+  Serial.println(Distnce);
   
+}
 
+
+float get_ultrasonic_reading(int Trigger ,int Echo)
+{
+  long duration;
+  float distanceCm;
+  digitalWrite(Trigger, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trigger, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(Echo, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  Serial.println("=================") ;
+  Serial.println("Function reading :") ;
+  Serial.println(distanceCm) ;
+  Serial.println("=================") ;
+  
+  return distanceCm ;
+}
+
+void Ultrasonic_init() {
+ 
+    pinMode(ULTRASONIC_F1_TRIGGER, OUTPUT); // Sets the trigPin as an Output
+    pinMode(ULTRASONIC_F1_ECHO, INPUT); // Sets the echoPin as an Input
+
+    pinMode(ULTRASONIC_B1_TRIGGER, OUTPUT); // Sets the trigPin as an Output
+    pinMode(ULTRASONIC_B1_ECHO, INPUT); // Sets the echoPin as an Input
 
 }
