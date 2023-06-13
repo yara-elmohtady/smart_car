4da385d14a2e9b085f6f04a6fbb0c437cf62978b
mahmamdouh <mahmoud.elmohtady@gmail.com>
2023-06-13 22:31:58 +0200

    GSM Data Trial

--- software/Drivers/GSM_Driver/GSM_Driver_Test/GSM.h
+++ software/Drivers/GSM_Driver/GSM_Driver_Test/GSM.h
@@ -2,8 +2,6 @@
 #ifndef GSM_H
 #define GSM_H
 
-#define Battary_full_voltage 12
-#define ADC_Buffer 4095
 
 extern void GSM_Init(SoftwareSerial GEM_Serial);
 extern void GSM_Send_comand(String text);
