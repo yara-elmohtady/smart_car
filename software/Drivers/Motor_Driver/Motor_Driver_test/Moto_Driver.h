0bd0251f2063b6accb2f25ae10be10a05c69793f
yara-elmohtady <128075776+yara-elmohtady@users.noreply.github.com>
2023-06-17 12:44:45 -1000

    motor driver

--- software/Drivers/Motor_Driver/Motor_Driver_test/Moto_Driver.h
+++ software/Drivers/Motor_Driver/Motor_Driver_test/Moto_Driver.h
@@ -7,13 +7,13 @@
 
 #define Motor_speed 150
 
-extern void Motor_Driver_init();
-extern void Car_motion_forward();
-extern void Car_motion_backword();
-extern void Car_motion_turn_right();
-extern void Car_motion_turn_left();
-extern void Car_motion_stop();
-extern void Car_motion_manipulation(int angle) ;
+void Motor_Driver_init();
+void Car_motion_forward();
+void Car_motion_backword();
+void Car_motion_turn_right();
+void Car_motion_turn_left();
+void Car_motion_stop();
+void Car_motion_manipulation(int angle) ;
 
 void Car_motion_acceleration_full();
 void Car_motion_acceleration_Right();
--- software/Drivers/Motor_Driver/Motor_Driver_test/Motor_Driver_test.ino
+++ software/Drivers/Motor_Driver/Motor_Driver_test/Motor_Driver_test.ino
@@ -11,23 +11,23 @@ void loop() {
     // put your main code here, to run repeatedly:
     Serial.println("Car_motion_forward");
     Car_motion_forward();
-    delay(1000);
+    delay(10000);
     Serial.println("Car_motion_stop");
     Car_motion_stop();
     Serial.println("Car_motion_backword");
     Car_motion_backword();
-    delay(1000);
+    delay(10000);
     Serial.println("Car_motion_stop");
     Car_motion_stop();
     Serial.println("Car_motion_turn_right");
     Car_motion_turn_right();
-    delay(1000);
+    delay(10000);
     Serial.println("Car_motion_stop");
     Car_motion_stop();
     Serial.println("Car_motion_turn_left");
     Car_motion_turn_left();
-    delay(1000);
+    delay(10000);
     Serial.println("Car_motion_stop");
     Car_motion_stop();
-    delay(1000);
+    delay(10000);
 }
