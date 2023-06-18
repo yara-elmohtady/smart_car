941770fe1228ffea735d73bf4d9243a835395db5
mahmamdouh <mahmoud.elmohtady@gmail.com>
2023-06-18 04:21:40 +0200

    moto1

--- software/Drivers/Motor_Driver/Motor_Driver_test/Pinout.h
+++ software/Drivers/Motor_Driver/Motor_Driver_test/Pinout.h
@@ -1,16 +1,16 @@
 // Pinout defenitions
 
 
-#define Motor_Driver_Right_PWM 2 // D4 PWM Left wheel ENA
-#define Motor_Driver_Left_PWM 14  // D5 pwm Right wheel ENB
+#define Motor_Driver_Right_PWM D4 // D4 PWM Left wheel ENA
+#define Motor_Driver_Left_PWM D5  // D5 pwm Right wheel ENB
 
 
 // Motor R F
-#define Motor_Driver_Right_Dir_in1  16 // d0 in2
-#define Motor_Driver_Right_Dir_in2  5 // d1  in1
+#define Motor_Driver_Right_Dir_in1  D0 // d0 in2
+#define Motor_Driver_Right_Dir_in2  D1 // d1  in1
 
 // Motor R B
-#define Motor_Driver_Left_Dir_in3   4 // d2  in3
-#define Motor_Driver_Left_Dir_in4   0 // d3  in4
+#define Motor_Driver_Left_Dir_in3   D2 // d2  in3
+#define Motor_Driver_Left_Dir_in4   D3 // d3  in4
 
 
